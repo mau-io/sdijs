ab1275899ee8b4c94499965754bcb9ecb3f652fe
mau-io <maurice222@outlook.com>
2020-04-26 22:45:08 -0500

    Refactor index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+const v8 = require('v8');
+
+const structuredClone = obj => {
+  return v8.deserialize(v8.serialize(obj));
+};
+
 const ANSI = {
   reset: '\033[0m',
-
   // Text color
   red: '\033[31m',
   green: '\033[32m',
-  yellow: '\033[33m',
   blue: '\033[34m',
-  magenta: '\033[35m',
-  cyan: '\033[36m',
-  white: '\033[37m',
 }
 module.exports = class DependencyInjection {
   
   constructor({verbose = false } = {}) {
+    this._verbose = verbose;
     this._services = new Map();
     this._singletons = new Map();
-    this._verbose = verbose;
   }
 
   resolve(name) {
@@ -34,81 +35,90 @@ module.exports = class DependencyInjection {
 
   register(service, alias, singleton){
     const type = this._isClass(service) ? 'class' : typeof service;
-    let name = alias ? alias : this._formatName(service.name);
-    this._services.set(name, {type, service, singleton})
+    const name = alias || this._formatName(service.name);
+    this._services.set(name, {
+      type, 
+      service, 
+      singleton
+    });
   }
 
   getInjector(module, name) {
 
-		let container = this;
-		
-		let paramParser = new Proxy({}, {
-			// The "get" handler is invoked whenever a get-call for
-			// "injector.*" is made. We make a call to an external service
-			// to actually hand back in the configured service. The proxy
-			// allows us to bypass parsing the function params using
-			// taditional regex or even the newer parser.
-			get: (target, key) => { 
-        container._log(key, module);
-				return container.resolve(key);
-			},
+    const paramParser = new Proxy({}, {
+      // The "get" handler is invoked whenever a get-call for
+      // "injector.*" is made. We make a call to an external service
+      // to actually hand back in the configured service. The proxy
+      // allows us to bypass parsing the function params using
+      // taditional regex or even the newer parser.
+      get: (target, key) => { 
+        this._verbose && this._log(key, module);
+        return this.resolve(key);
+      },
 
-			// You shouldn't be able to set values on the injector.
-			set: (target, key, value) => {
-				throw new Error(`Don't try to set ${key}!`);
-			}
+      // You shouldn't be able to set values on the injector.
+      set: (target, key, value) => {
+        throw new Error(`Don't try to set ${key}!`);
+      }
 
     });
  
-    if(container._isFunction(module.service)) {
+    if(this._isFunction(module.service)) {
       if(module.singleton) {
-        const singletonInstance = container._singletons.get(name);
+        const singletonInstance = this._singletons.get(name);
 
         if(singletonInstance) {
           return singletonInstance;
         } else {
           // Create Instance
-          const newSingletonInstance = container._isClass(module.service) ? 
-            new module.service(paramParser) : module.service.call(null, paramParser);
+          const newSingletonInstance = this._isClass(module.service) ? 
+            new module.service(paramParser) : 
+            module.service.call(null, paramParser);
           // Save Instance
-          container._singletons.set(name, newSingletonInstance);
+          this._singletons.set(name, newSingletonInstance);
           // Send Instance
           return newSingletonInstance;
         }
   
       }else{
         // Create and Send Instance
-        return container._isClass(module.service) ? 
-          new module.service(paramParser) : module.service.call(null, paramParser);
+        return  this._isClass(module.service)   ? 
+                new module.service(paramParser) : 
+                module.service.call(null, paramParser);
       }
     } else {
 
       return  module.singleton  ? 
-              module.service    : // Send reference object 
-        { ...module.service }     // Send a copy object with ES6 style
+              module.service    : // Send reference object
+              structuredClone(module.service) // Send a deep copy object
     }
-  	
+    
   }
+
   _isClass(definition) {
-		return typeof definition === 'function'
+    return typeof definition === 'function'
       && /^class\s/.test(Function.prototype.toString.call(definition));
   }
   _isFunction(definition) {
-		return typeof definition === 'function';
+    return typeof definition === 'function';
   }
+
   _formatName(string) {
     return string.charAt(0).toLowerCase() + string.substr(1);
   }
+
   _log(name, module) {
-    if(this._verbose){
-      try {
-        let from = `${this._services.get(name).singleton ? ANSI.cyan + '[S]' : '[T]'} (${this._services.get(name).type}) ${name}`;  
-        let to = `${module.singleton ? (ANSI.cyan + '[S]') : '[T]'} ${module.service.name}`;
-        console.info(`${ANSI.white} ${from} ${ANSI.blue} ${'>'.padStart(20 - name.length - this._services.get(name).type.length, '-')} ${ANSI.green} ${to} ${ANSI.reset}`);
-      } catch (error) {
-        
-      }
-      
+    try {
+      const T = `${ANSI.blue} [T] ${ANSI.reset}`;
+      const S = `${ANSI.green} [S] ${ANSI.reset}`;
+      const type = this._services.get(name).type;
+      const from = `${this._services.get(name).singleton ? S : T} (${type}) ${name}`;  
+      const to = `${module.singleton ? S : T} ${module.service.name}`;
+      const arrow =  ANSI.red + '>'.padStart(25 - name.length - this._services.get(name).type.length, '-')
+      console.info(`${from} ${arrow} ${to}`);
+    } catch (error) {
+      console.error(error);
     }
   }
+
 }
